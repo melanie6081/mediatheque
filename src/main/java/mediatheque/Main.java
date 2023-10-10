8d4b24b4de5bb7390552e27e6bb08dacd5cc64e6
mferon <melanie.feron@etud.univ-jfc.fr>
2023-10-10 12:27:41 +0200

    corrections

--- src/main/java/mediatheque/Main.java
+++ src/main/java/mediatheque/Main.java
@@ -7,24 +7,11 @@ public class Main {
         mediatheque.addItem( new Book("J.R.R. Tolkien", "Le seigneur des anneaux"));
         mediatheque.addItem( new Book("Philip K. Dick", "Le Maître du haut chateau"));
         mediatheque.addItem( new CD(12, "Sergeant Peppers"));
-        //mediatheque.printCatalog();
-        //mediatheque.printOnlyBooks();
-        //mediatheque.printOnlyCDs();
-        ItemVisitor v= new CatalogPrinter();
-        ItemVisitor vCD= new CDPrinter();
-        ItemVisitor vBook= new BookPrinter();
 
-        for (Item i : mediatheque.liste()){
-            i.accept(v);
-        }
+        mediatheque.printCatalog();
+        mediatheque.printOnlyCDs();
+        mediatheque.printOnlyBooks();
 
-        for (Item i : mediatheque.liste()){
-            i.accept(vCD);
-        }
-
-        for (Item i : mediatheque.liste()){
-            i.accept(vBook);
-        }
 
     }
 }
--- src/main/java/mediatheque/Mediatheque.java
+++ src/main/java/mediatheque/Mediatheque.java
@@ -6,32 +6,40 @@ import java.util.List;
 
 public class Mediatheque {
 	private final List<Item> items = new LinkedList<>();
+	private final ItemVisitor v= new CatalogPrinter();
+	private final ItemVisitor vCD= new CDPrinter();
+	private final ItemVisitor vBook= new BookPrinter();
 
 	public void addItem(Item i) {
 		items.add(i);
 	}
 	
 	public void printCatalog() {
-		for (Item i : items)
-			i.print();
+		//for (Item i : items)
+		//	i.print();
+		for (Item i : items){
+			i.accept(v);
+		}
 	}
 	
 	public void printOnlyBooks() {
-		throw new UnsupportedOperationException("Not supported yet."); 
+		// throw new UnsupportedOperationException("Not supported yet.");
 		/*
 		//avec instanceof
 		for (Item i : items)
 			if (i instanceof Book)
 				System.out.println(i);
 		*/
+		for (Item i : items){
+			i.accept(vBook);
+		}
 	}
 
 	public void printOnlyCDs() {
-		throw new UnsupportedOperationException("Not supported yet."); 
+		for (Item i : items){
+			i.accept(vCD);
+		}
 	}
 
-	public List<Item> liste(){
-		return items;
-	}
 
 }
